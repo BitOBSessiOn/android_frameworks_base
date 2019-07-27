8cf716ef90f2cb7a122d6f2a86e93463dd1c395b
mTresk <djtresk@gmail.com>
2019-07-27 16:14:56 +0200

    Revert "Stock Recents/Overview icon pack support [2/3]"
    This reverts commit 9ebc3ddc832fcef4a4e2dba792e173a79e0d7ef0.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4845,12 +4845,6 @@ public final class Settings {
         private static final Validator FORCE_FULLSCREEN_CUTOUT_APPS_VALIDATOR = ANY_STRING_VALIDATOR;
 
         /**
-         * Whether to use icon pack for Recents
-         * @hide
-         */
-        public static final String RECENTS_ICON_PACK = "recents_icon_pack";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
